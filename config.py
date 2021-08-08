7c0680f65617f02d6900fd7c12600953163ff026
pl-pr-pm <pl.pr.pm.12@gmail.com>
2021-08-08 16:44:01 +0900

    認識ワードによる操作分岐を作成

---
+++ config.py
@@ -0,0 +1,3 @@
+# 音声認識に利用するデバイスのマイク名
+# speech_recognition.Microphone.list_microphone_names() を実行しマイク名を把握する
+MIC_NAME=''
--- lib/speechRecognition/speech.py
+++ lib/speechRecognition/speech.py
@@ -1,30 +1,54 @@
+import sys
+sys.path.append('../../')
+
 import speech_recognition as sr
+from lib.util import _logger_setup
+import config
 import logging
 
+class Error(Exception):
+    pass
+
 class speechRecognition():
+
     def __init__(self, func):
         # self.function = function
+        self.logger = _logger_setup(logging.DEBUG)
+        #self.mic_name = config.MIC_NAME
+        self.mic_name = 'Built-in Microphone'
         self.recognizer = sr.Recognizer()
-        self.mic = sr.Microphone()
-        
-    
+        self.mic = sr.Microphone(device_index=(self._ret_mic_index()))
+
     def run(self):
         # obtain audio from the microphone
-        
         while True:
-               
-        with self.mic as source:
-           print("Say something!")
-           audio = self.recognizer.listen(source)
+           self.logger.info("Start Speech Recognition")
+           with self.mic as source:
+              self.recognizer.adjust_for_ambient_noise(source, duration=0.5) # ノイズ軽減 @https://realpython.com/python-speech-recognition/#the-effect-of-noise-on-speech-recognition
+              audio = self.recognizer.listen(source)
+           try:
+              # 'stop'を認識したら終了する
+              word = self.recognizer.recognize_sphinx(audio)
+              if word.lower() in ['stop', 'stop it']:
+                  self.logger.debug("Speech Recognition is over.")
+                  break
+              # ./lib/python3.6/site-packages/speech_recognition/pocketsphinx-data/en-US/pronounciation-dictionary.dict
+              # 上記ファイルに 'kawaii' を追加したが認識してくれない。そのため、かわいい を発した際に Sphinx が認識したワードを用いる
+              elif word.lower() in ['kawaii', 'corey', 'hawaii']:
+                  self.logger.debug("The function activate...")
+                  # self.func.run()
+              else:
+                  self.logger.debug("Sphinx thinks you said " + word)
+                  self.logger.debug("Still continue")
+           except sr.UnknownValueError:
+              self.logger.error("Sphinx could not understand audio")
+           except sr.RequestError as e:
+              self.logger.error("Sphinx error; {0}".format(e))
 
-    # recognize speech using Sphinx
+    """ユーザが利用するマイクのインデックスを返却する"""
+    def _ret_mic_index(self):
+        print(sr.Microphone.list_microphone_names())
         try:
-           print("Sphinx thinks you said " + self.recognizer.recognize_sphinx(audio))
-        except sr.UnknownValueError:
-           print("Sphinx could not understand audio")
-        except sr.RequestError as e:
-           print("Sphinx error; {0}".format(e))
-   
-   def __logger_setup(self):
-          
-
+           return sr.Microphone.list_microphone_names().index(self.mic_name)
+        except Exception as e:
+           raise Error('configにて選択したマイクは存在しません。 Key:MIC_NAME')
