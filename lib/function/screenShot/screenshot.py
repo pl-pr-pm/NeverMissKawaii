5607f9488b701da347d9a4f7580c82f26f9889b1
pl-pr-pm <pl.pr.pm.12@gmail.com>
2021-08-08 18:10:18 +0900

    スクリーンショット機能を作成

---
+++ lib/function/screenShot/screenshot.py
@@ -0,0 +1,46 @@
+import sys
+sys.path.append('../../../')
+
+from lib.util import _logger_setup, _get_now_str
+import config
+import logging
+import subprocess
+import os
+
+"""
+自作スクリーンショットクラス
+ググるとpython でスクリーンショットのライブラリは以下三つがヒットした
+
+- PIL https://github.com/python-pillow/Pillow
+- pyscreeze https://github.com/asweigart/pyscreeze/tree/master/pyscreeze
+- pyautogui
+
+pyautogui は、内部で pyscreeze を利用し、 pyscreezeは、PIL を利用している。
+PILは、 subprocess でscreenshotを実行している。その後、PILの関数を利用し、トリミングを行なっている。
+
+NMKは、リアルタイムで早急なスクリーンショット実行が必要なため純粋なスクリーンショット機能を自作で作成する。
+（とは言っても、subprocess で screencapture を実行するのみではあるが）
+"""
+
+class ScreenShot():
+
+    def __init__(self):
+        self.logger = _logger_setup(logging.DEBUG)
+        self.output_path = config.OUTPUT_PATH + util._get_now_str() + '.jpg'
+    
+    """スクリーンショット実行"""
+    @classmethod
+    def do(cls):
+        # -x → サウンドなし
+        try:
+           subprocess.call(["screencapture", "-x", cls.output_path])
+           cls.logger.debug('スクリーンショット取得完了。保存先：' + cls.output_path)
+        
+        except Exception as e:
+           cls.logger.error('スクリーンショット取得に失敗しました')
+           cls.logger.error(e)
+           cls._delete()
+    
+    """作成ファイルを削除"""
+    def _delete(self):
+        os.unlink(self.output_path)
