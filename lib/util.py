2e8bd6c5991cac4331c8efaba75ed4ae3b0a11cf
pl-pr-pm <pl.pr.pm.12@gmail.com>
2021-08-08 18:11:44 +0900

    ファイル削除機能を追加

---
+++ lib/util.py
@@ -0,0 +1,12 @@
+import logging
+import datetime
+
+def _logger_setup(level):
+    logger = logging.getLogger(__name__)
+    logger.setLevel(level)
+    return logger
+
+def _get_now_str():
+    now = datetime.datetime.now()
+    now_str = now.strftime('%Y%m%d')
+    return now_str
