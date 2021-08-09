c3d4c3306c1fb57ad7b83804e3664e0204a0504c
pl-pr-pm <pl.pr.pm.12@gmail.com>
2021-08-09 10:52:27 +0900

    ワードの一致関数を作成

--- lib/util.py
+++ lib/util.py
@@ -1,5 +1,6 @@
 import logging
 import datetime
+import re
 
 def _logger_setup(level):
     logger = logging.getLogger(__name__)
@@ -10,3 +11,13 @@ def _get_now_str():
     now = datetime.datetime.now()
     now_str = now.strftime('%Y%m%d%H%M%S')
     return now_str
+
+
+"複数のパターンにワードが一致した場合Trueを返却、そうでなければFalseを返却"
+def _match_word(patterns, word):
+    for pattern in patterns:
+        repattern = re.compile(pattern)
+        result = repattern.search(word)
+        if result:
+            return True
+    return False
