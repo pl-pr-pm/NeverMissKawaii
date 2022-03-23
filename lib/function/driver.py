67a8346e804eee09b71c90048080cb847ce5ad1a
pl-pr-pm <pl.pr.pm.12@gmail.com>
2022-03-23 13:28:46 +0900

    不要な出力を削除

--- lib/function/driver.py
+++ lib/function/driver.py
@@ -1,11 +1,9 @@
 import sys
 sys.path.append('../../')
-print(sys.path)
 
 """
 ScreenShotクラスと、各FunctionクラスのIF（インターフェース）クラス
 各Functionクラスの改修の影響を抑えるために作成
-（を考えていたが、現在のところそこまで価値がないように思えている）
 """
 
 class Driver():
